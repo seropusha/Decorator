24cb07e97c09e0c2ce3d632aa0ba43e665870398
Mikhail Vasilev <iwheelbuy@MacBook-Pro-Mikhail.local>
2017-05-01 14:09:59 +0700

    0.1.0

--- Decorator/Classes/Decorator.swift
+++ Decorator/Classes/Decorator.swift
@@ -1,24 +1,24 @@
 import Foundation
 
-typealias Decoration<T> = (T) -> Void
+public typealias Decoration<T> = (T) -> Void
 
-struct Decorator<T> {
+public struct Decorator<T> {
     
     let object: T
     
-    func apply(_ decorations: Decoration<T>...) -> Void {
+    public func apply(_ decorations: Decoration<T>...) -> Void {
         decorations.forEach({ $0(object) })
     }
 }
 
-protocol DecoratorCompatible {
+public protocol DecoratorCompatible {
     
     associatedtype DecoratorCompatibleType
     
     var decorator: Decorator<DecoratorCompatibleType> { get }
 }
 
-extension DecoratorCompatible {
+public extension DecoratorCompatible {
     
     var decorator: Decorator<Self> {
         return Decorator(object: self)
